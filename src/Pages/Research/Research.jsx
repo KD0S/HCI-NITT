46642b0742b633de7c46a5c8edabc1ef89c62c19
hariesh-s <shariesh2002@gmail.com>
2023-12-26 23:34:30 +0400

    refine: research page

--- src/Pages/Research/Research.jsx
+++ src/Pages/Research/Research.jsx
@@ -1,43 +1,54 @@
-import './Research.css'
-import Footer from "../../Components/Footer/Footer"
-import { Header } from "../../Components/Header/Header"
+import "./Research.css";
+import Footer from "../../Components/Footer/Footer";
+import { Header } from "../../Components/Header/Header";
 import { IoIosFlask } from "react-icons/io";
+import { IconContext } from "react-icons";
 
 const Research = () => {
-    return (
-        <div>
-            <Header></Header>
-            <h1 className="research-content-title">What we Do
-                <div className='research-content-icon'><IoIosFlask></IoIosFlask></div>
-            </h1>
-            <div className="research-content-body">
-                <div>
-                    <h2>Our Research Areas</h2>
-                    <ul>
-                        <li>Human Computer Interaction (e-learning)</li>
-                        <li>Brain Computer Interfaces (algorithms, devices, systems, applications)</li>
-                        <li>Neural Signal Processing</li>
-                        <li>Neural Image Processing</li>
-                        <li>Machine learning</li>
-                        <li>Deep Learning</li>
-                        <li>Artificial Intelligence</li>
-                        <li>Computer Architecture</li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>Our Research Topics</h2>
-                    <ul>
-                        <li>Emotional Intelligence as personal context</li>
-                        <li>Emotions in humans</li>
-                        <li>Cognitive load in tasks</li>
-                        <li>Imagined Speech in paralyzed patients</li>
-                        <li>Motor imaginary in disabled patients</li>
-                    </ul>
-                </div>
-            </div>
-            <Footer></Footer>
+  return (
+    <div className="research-main">
+      <Header></Header>
+      <div className="research-content-header">
+        <h1>WHAT WE DO</h1>
+        <IconContext.Provider value={{ className: "research-content-icon" }}>
+          <IoIosFlask />
+        </IconContext.Provider>
+      </div>
+      <div className="research-content-body">
+        <div className="research-content-body-list-container">
+          <h2 className="research-content-body-list-title">
+            Our Research Areas
+          </h2>
+          <ul className="research-content-body-list">
+            <li>Human Computer Interaction (e-learning)</li>
+            <li>
+              Brain Computer Interfaces (algorithms, devices, systems,
+              applications)
+            </li>
+            <li>Neural Signal Processing</li>
+            <li>Neural Image Processing</li>
+            <li>Machine learning</li>
+            <li>Deep Learning</li>
+            <li>Artificial Intelligence</li>
+            <li>Computer Architecture</li>
+          </ul>
         </div>
-    )
-}
+        <div className="research-content-body-list-container">
+          <h2 className="research-content-body-list-title">
+            Our Research Topics
+          </h2>
+          <ul className="research-content-body-list">
+            <li>Emotional Intelligence as personal context</li>
+            <li>Emotions in humans</li>
+            <li>Cognitive load in tasks</li>
+            <li>Imagined Speech in paralyzed patients</li>
+            <li>Motor imaginary in disabled patients</li>
+          </ul>
+        </div>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
+};
 
-export default Research
+export default Research;
