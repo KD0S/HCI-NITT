7395db0c0560236847128ee887c9fbd5574c945e
hariesh-s <shariesh2002@gmail.com>
2023-12-26 23:51:28 +0400

    minor carousel changes

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,68 +1,62 @@
 import "./Carousel.css";
 import React, { useState } from "react";
 import { imgSlides } from "./ImgSlides";
-import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 const Carousel = () => {
-    const [slide, setSlide] = useState(0);
+  const [slide, setSlide] = useState(0);
 
-    const nextSlide = () => {
-        setSlide((slide + 1) % imgSlides.length);
-    };
+  const nextSlide = () => {
+    setSlide((slide + 1) % imgSlides.length);
+  };
 
-    const prevSlide = () => {
-        setSlide(slide === 0 ? imgSlides.length - 1 : slide - 1);
-    };
+  const prevSlide = () => {
+    setSlide(slide === 0 ? imgSlides.length - 1 : slide - 1);
+  };
 
-    return (
-        <div className="carousel">
-            <BsArrowLeftCircleFill
-                className="arrow arrow-left"
-                onClick={prevSlide}
-            ></BsArrowLeftCircleFill>
+  return (
+    <div className="carousel">
+      <IoIosArrowBack className="arrow arrow-left" onClick={prevSlide} />
 
-            <div className="slider">
-                {imgSlides.map((item, idx) => {
-                    return (
-                        <img
-                            src={item.src}
-                            alt={item.alt}
-                            key={idx}
-                            className="img-slider"
-                            style={{ translate: `${-100 * slide}%` }}
-                        ></img>
-                    );
-                })}
-            </div>
+      <div className="slider">
+        {imgSlides.map((item, idx) => {
+          return (
+            <img
+              src={item.src}
+              alt={item.alt}
+              key={idx}
+              className="img-slider"
+              style={{ translate: `${-100 * slide}%` }}
+            ></img>
+          );
+        })}
+      </div>
 
-            <div className="overlay">
-                {/* <h2>Welcome to</h2> */}
-                <h1>Human-Computer Interface Lab</h1>
-                <h1 style={{ color: 'lightskyblue' }}>@National Institute of Technology, Tiruchirappalli</h1>
-            </div>
+      <div className="overlay">
+        <h2>Welcome to</h2>
+        <h1>Human-Computer Interface Lab</h1>
+        <h1 style={{ color: "#6783e8" }}>
+          @National Institute of Technology, Tiruchirappalli
+        </h1>
+      </div>
 
-            <BsArrowRightCircleFill
-                className="arrow arrow-right"
-                onClick={nextSlide}
-            ></BsArrowRightCircleFill>
+      <IoIosArrowForward className="arrow arrow-right" onClick={nextSlide} />
 
-            <span className="indicators">
-                {imgSlides.map((_, idx) => {
-                    return (
-                        <button
-                            key={idx}
-                            onClick={() => setSlide(idx)}
-                            className={
-                                slide === idx
-                                    ? "indicator"
-                                    : "indicator indicator-hidden"
-                            }
-                        ></button>
-                    );
-                })}
-            </span>
-        </div>
-    );
+      <span className="indicators">
+        {imgSlides.map((_, idx) => {
+          return (
+            <button
+              key={idx}
+              onClick={() => setSlide(idx)}
+              className={
+                slide === idx ? "indicator" : "indicator indicator-hidden"
+              }
+            ></button>
+          );
+        })}
+      </span>
+    </div>
+  );
 };
 
 export default Carousel;
