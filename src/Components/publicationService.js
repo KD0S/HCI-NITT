543b97e8c43e5c3d17ed5e52d82edab6934ee173
Krishnadas Nair <krishnadas.nair02@gmail.com>
2023-12-27 15:36:45 +0530

    Changes to Api routes

--- src/Components/publicationService.js
+++ src/Components/publicationService.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const baseUrl = 'http://localhost:3001/api/publications'
+const baseUrl = 'https://hci-backend-94fk.onrender.com/api/publications'
 
 const getAll = async () => {
     const response = await axios.get(baseUrl)
