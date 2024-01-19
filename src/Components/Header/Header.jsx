89b7bf8761826d283ece78709f9f0aa7da6187ba
hariesh-s <shariesh2002@gmail.com>
2024-01-19 14:58:24 +0400

    dropdown

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,42 +1,67 @@
+import { IconContext } from "react-icons";
+import { IoIosArrowDropdown } from "react-icons/io";
+import { Link, NavLink } from "react-router-dom";
 import "./Header.css";
-import { NavLink } from "react-router-dom";
 
 export const Header = () => {
   return (
     <div className="header-main">
-      <NavLink style={
-        {
-          textDecoration: 'none'
-        }
-      } className={({ isActive }) =>
-        isActive ? "active" : ""
-      } to="/admin/login">
-        <h1 className="app-title">HCI<strong style={{ color: '#5c7c89' }}>@</strong>NITT</h1>
+      <NavLink
+        style={{
+          textDecoration: "none",
+        }}
+        className={({ isActive }) => (isActive ? "active" : "")}
+        to="/admin/login"
+      >
+        <h1 className="app-title">
+          HCI<strong style={{ color: "#5c7c89" }}>@</strong>NITT
+        </h1>
       </NavLink>
       <div className="headings">
-        <NavLink className={({ isActive }) =>
-          isActive ? "active" : ""
-        } to="/">Home</NavLink>
-        <NavLink className={({ isActive }) =>
-          isActive ? "active" : ""
-        } to="/members">Members</NavLink>
-        <NavLink className={({ isActive }) =>
-          isActive ? "active" : ""
-        } to="/research">Research</NavLink>
-        <NavLink className={({ isActive }) =>
-          isActive ? "active" : ""
-        } to="/projects">Projects</NavLink>
-        <NavLink className={({ isActive }) =>
-          isActive ? "active" : ""
-        } to="/publications">Publications</NavLink>
-        <NavLink className={({ isActive }) =>
-          isActive ? "active" : ""
-        } to="/collaborate">Collaborate</NavLink>
-        <NavLink className={({ isActive }) =>
-          isActive ? "active" : ""
-        } to="/contact">Contact Us</NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "active" : "")}
+          to="/"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "active" : "")}
+          to="/members"
+        >
+          Members
+        </NavLink>
+        <div className="heading-dropdown-main">
+          <p className="heading-dropdown-trigger">Our Work</p>
+          <IconContext.Provider value={{ className: "heading-dropdown-icon" }}>
+            <IoIosArrowDropdown />
+          </IconContext.Provider>
+          <ul className="heading-dropdown-content">
+            <li>
+              <Link to="/research">Research</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/publications">Publications</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="heading-dropdown-main">
+          <p className="heading-dropdown-trigger">Let's Connect</p>
+          <IconContext.Provider value={{ className: "heading-dropdown-icon" }}>
+            <IoIosArrowDropdown />
+          </IconContext.Provider>
+          <ul className="heading-dropdown-content">
+            <li>
+              <Link to="/collaborate">Collaborate</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
+          </ul>
+        </div>
       </div>
-
     </div>
   );
 };
